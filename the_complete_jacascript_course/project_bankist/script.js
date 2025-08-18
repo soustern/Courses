f9c65e6eab6366f9790ec35ece025ee8ba5c1f97
soustern <rafaelantoniassivicechio@gmail.com>
2025-08-18 15:51:57 -0300

    Create Lecture 157 notes

--- the_complete_jacascript_course/project_bankist/script.js
+++ the_complete_jacascript_course/project_bankist/script.js
@@ -86,12 +86,17 @@ const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
-]);
+]); 
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const eurToUsd = 1.1;
+
+const movementsUsd = movements.map(movement => movement * eurToUsd);
+
 /////////////////////////////////////////////////
 
+//#region console training
 
 // Slice method -----------------------------------------------------------------------------------------------------
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -175,3 +180,4 @@ currenciesUnique.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
 });
 
+//#endregion
